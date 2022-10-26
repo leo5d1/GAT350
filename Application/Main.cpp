477e08a82175cd4ff811f816d8a24f1d0e20488f
leo5d1 <80261451+leo5d1@users.noreply.github.com>
2022-10-26 13:40:15 -0600

    Cleaned out Main

--- Application/Main.cpp
+++ Application/Main.cpp
@@ -2,50 +2,6 @@
 #include <vector>
 #include <iostream>
 
-float vertices[] = {
-	-0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
-	 0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
-	 0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
-	 0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
-	-0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
-	-0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
-
-	-0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
-	 0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
-	 0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
-	 0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
-	-0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
-	-0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
-
-	-0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
-	-0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
-	-0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
-	-0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
-	-0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
-	-0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
-
-	 0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
-	 0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
-	 0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
-	 0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
-	 0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
-	 0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
-
-	-0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
-	 0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
-	 0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
-	 0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
-	-0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
-	-0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
-
-	-0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
-	 0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
-	 0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
-	 0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
-	-0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
-	-0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f
-};
-
 int main(int argc, char** argv)
 {
 	LOG("Application Started...");
@@ -65,39 +21,6 @@ int main(int argc, char** argv)
 	// load scene 
 	auto scene = c14::g_resources.Get<c14::Scene>("scenes/basic_Lit.scn");
 
-	// create vertex buffer
-	//std::shared_ptr<c14::VertexBuffer> vb = c14::g_resources.Get<c14::VertexBuffer>("box");
-	//vb->CreateVertexBuffer(sizeof(vertices), 36, vertices);
-	//vb->SetAttribute(0, 3, 8 * sizeof(float), 0);
-	//vb->SetAttribute(1, 3, 8 * sizeof(float), 3 * sizeof(float));
-	//vb->SetAttribute(2, 2, 8 * sizeof(float), 6 * sizeof(float));
-
-	//// create program
-	//std::shared_ptr<c14::Program> program = c14::g_resources.Get<c14::Program>("Shaders/basic.prog", GL_PROGRAM);
-	//program->Link();
-	//program->Use();
-
-	////auto spot = c14::g_resources.Get<c14::Model>("models/spot.obj");
-	//auto ogre = c14::g_resources.Get<c14::Model>("models/ogre.obj");
-
-	// create material 
-	//std::shared_ptr<c14::Material> material = c14::g_resources.Get<c14::Material>("materials/ogre.mtrl");
-	//std::shared_ptr<c14::Material> material = c14::g_resources.Get<c14::Material>("materials/spot.mtrl");
-	//material->Bind();
-
- 	//material->GetProgram()->SetUniform("scale", 0.5f);
-	//material->GetProgram()->SetUniform("tint", glm::vec3{ 1, 0, 0 });
-	
-	//glm::mat4 model{ 1 };
-	//glm::mat4 projection = glm::perspective(45.0f, c14::g_renderer.GetWidth() / (float)c14::g_renderer.GetHeight(), 0.01f, 100.0f);
-
-	//glm::vec3 cameraPosition{ 0, 0, 2 };
-	//float speed = 3;
-
-	//std::vector<c14::Transform> transforms;
-
-	//transforms.push_back({ { 0, 0, 0}, {0, 180, 0} });
-
 	bool quit = false;
 	while (!quit)
 	{
@@ -110,8 +33,6 @@ int main(int argc, char** argv)
 		{
 			actor->m_transform.rotation.y += c14::g_time.deltaTime * 90.0f;
 		}
-
-		//glm::mat4 view = glm::lookAt(cameraPosition, cameraPosition + glm::vec3{ 0, 0, -1 }, glm::vec3{ 0, 1, 0 });
 
 		scene->Update();
 
